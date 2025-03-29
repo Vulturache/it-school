b35c7dac53b9016d514153f2dbee6930ad41889c
Alexandru Sandu <sanduvulturu@gmail.com>
2025-03-29 23:29:39 +0200

    tema 6

--- firstproject/firstproject/firstproject/firstproject.cpp
+++ firstproject/firstproject/firstproject/firstproject.cpp
@@ -7,60 +7,98 @@ using namespace std;
 #include <map>
 #include <numeric>
 
-void afisare(vector<int>& v) {
+// ex. 4
 
-	for (int el : v) {
-		cout << el << " ";
-	}
-	cout << endl;
+void swap_r(int& a, int& b)
+{
+	int temp = 0;
+	temp = a;
+	a = b;
+	b = temp;
+
 }
 
-//ex.7
+// ex. 5
+
+void swap_p(int* a, int* b)
+{
+	int temp = 0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+
 
 //int main() 
 //{
-//	vector<int> v = { 2,4,6,8,10 };
 //
-//	int suma = accumulate(v.begin(), v.end(), 0);
-//	cout << "Suma numerelor este= " << suma;
-//	
-//	//int suma = 0;
-//	//for (vector<int>::iterator i = v.begin(); i != v.end(); i++)
-//	//{
-//	//	suma = suma + *i;
-//	//}
-//	//cout << "Suma numerelor este= " << suma;
-//	
+//	int a;
+//	int b;
+//	cout << "Primul numar= "; cin >> a;
+//	cout << "Al doilea numar= "; cin >> b;
 //
-//	
+//	swap_r(a, b);
+//	cout << "Rezultatul inversari prin referinta este= ";
+//	cout << a << " ";
+//	cout << b;}
+
+//int main(){
 //
+//	int* a = new int;
+//	int* b = new int;
+//	cout << "Primul numar= "; cin >> *a;
+//	cout << "Al doilea numar= "; cin >> *b;
+//
+//	swap_p(a, b);
+//	cout << "Rezultatul inversari prin pointers este= ";
+//	cout << *a << " ";
+//	cout << *b;
 //}
 
+//ex. 2
 
-//ex.8
+struct Produs
+{
+	float pret;
+	string produs;
+	int nrdeproduse;
+};
 
-//int main() 
-//{
-//	vector<int> v = { 1,2,3,4,5 };
-//
-//	for (auto i = v.rbegin(); i != v.rend(); i++)
-//	{
-//		cout << " " << *i;
-//	}
-//	
-//
-//}
+void afisareProd(const vector<Produs> v)
+{
+	for (auto el : v)
+	{
+		cout << el.produs << " " << el.pret << " " << el.nrdeproduse << " " << endl;
+	}
+}
 
-//ex.9
+int main()
+{
+	
+	vector<Produs> v;
+	int n;
+	cout << "Introduceti numarul de produse in magazin= "; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		float temp2;
+		string temp3;
+		cout << "Introduceti numarul de produse in stoc= "; cin >> temp;
+		cout << "Introduceti pretul produselor= "; cin >> temp2;
+		cout << "Introduceti numele produsului= "; cin >> temp3;
+		Produs p1;
+		p1.nrdeproduse = temp;
+		p1.pret = temp2;
+		p1.produs = temp3;
+		v.push_back(p1);
 
-//int main()
-//{
-//	vector<int> v = { 1,2,2,3,4,4,5 };
-//	afisare(v);
-//	v.erase(v.begin() + 1);
-//	v.erase(v.begin() + 4);
-//	afisare(v);
-//
-//
-//
-//}
+	}
+	
+	afisareProd(v);
+	
+
+}
+
+
+
